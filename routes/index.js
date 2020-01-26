5239f5ca0dc4274e122d8318469bc45b12d2c863
hengki01 <hengki01.hengki.idn@gmail.com>
2020-01-26 02:47:35 +0000

    updating routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const routes = require('express').Router()
 
 routes.use('/user', require('./user'))
+routes.use('/fintech', require('./fintech'))
+routes.use('/application', require('./application'))
 
 module.exports = routes
