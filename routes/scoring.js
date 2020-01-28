4fb67d5e82c67e05d722cf1fde23f270a9ee2dd8
hengki01 <hengki01.hengki.idn@gmail.com>
2020-01-28 06:26:29 +0000

    updating scoring 2

--- routes/scoring.js
+++ routes/scoring.js
@@ -1,26 +1,30 @@
 const scoring = require('express').Router()
 const { authenticate } = require('../middlewares/auth')
+const { User, Application } = require('../models')
 const axios = require('axios')
 
 scoring.get('/', authenticate, async function(req, res, next) {
-	axios({
-		url: 'http://35.187.226.194',
-		method: 'GET',
-		data: {
-			amount: req.body.amount,
-			loan_term: req.body.loan_term,
-			salary: req.body.salary,
-			current_job: req.body.current_job,
-			date_of_birth: req.body.date_of_birth
-		}
-	})
-		.then((response) => {
-			console.log('response => ',response);
-			res.status(200).send(response.data)
-		})
-		.catch((err) => {
-			next(err)
-		})
+	try {
+        let user = req.user
+        const applications= await Application.find({user_id:user.id})
+	      	axios({
+				url: 'http://35.187.226.194',
+				method: 'POST',
+				data: {
+					amount: applications[0].amount,
+					loan_term: applications[0]/loan_term,
+					salary: user.salary,
+					current_job: user.current_job,
+					date_of_birth: user.date_of_birth
+				}
+			})
+				.then((response) => {
+					console.log('response => ',response);
+					res.status(200).send(response.data)
+				})
+    } catch (error) {
+      next(error)
+    }
 })
 
 module.exports = scoring
